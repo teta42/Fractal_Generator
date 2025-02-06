b1880fa40d51f9220f942848f44533256c107a8d
teta <covo@net.net>
2025-02-06 20:32:06 +0500

    Turn out

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Window import Window
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from Shader import Shader_manager
-import time
+import numpy as np
 
-width, height = 1280, 650
+width, height = 800, 600
 
 # Создание экземпляра GLFW
 this_window = Window(height=height, width=width)
@@ -15,6 +15,23 @@ shader = Shader_manager()
 # Используем шейдерную программу
 glUseProgram(shader.shader_program)
 
+# Координаты четырёх углов окна (в нормализованных координатах)
+vertices = np.array([
+    [-1.0, -1.0],  # Нижний левый угол
+    [ 1.0, -1.0],  # Нижний правый угол
+    [ 1.0,  1.0],  # Верхний правый угол
+    [-1.0,  1.0],  # Верхний левый угол
+], dtype=np.float32)
+
+# Создание VBO (Vertex Buffer Object)
+vbo = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
+glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
+
+# Настройка атрибутов вершин
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * 4, None)
+
 # Основной цикл
 try:
     while not glfw.window_should_close(this_window.window):
@@ -22,16 +39,13 @@ try:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         resolution = glGetUniformLocation(shader.shader_program, "resolution")
-        if resolution != -1:
-            glUniform2f(resolution, width, height)
+        glUniform2f(resolution, width, height)
 
-        time_uniform_location = glGetUniformLocation(shader.shader_program, "time")
-        if time_uniform_location != -1:
-            glUniform1f(time_uniform_location, time.time())
+        time_s = glGetUniformLocation(shader.shader_program, "time")
+        glUniform1f(time_s, glfw.get_time())
 
-
-        # Отрисовка прямоугольника
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        # Рисование четырёхугольника
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6)
 
         # Обновление окна
         glfw.swap_buffers(this_window.window)
--- shaders/fragment.shader
+++ shaders/fragment.shader
@@ -1,17 +1,19 @@
 #version 330 core
 
-out vec4 fragColor; // Выходной цвет фрагмента
-
 uniform vec2 resolution; // Разрешение окна (ширина, высота)
 uniform float time;      // Время в секундах
 
-void main() {
-    // Нормализуем координаты пикселя (от 0 до 1)
-    vec2 uv = gl_FragCoord.xy / resolution;
+// Выходной цвет
+out vec4 FragColor;
 
-    // Цвет пикселя, изменяющийся со временем
-    vec3 col = 0.5 + 0.5 * cos(time + uv.xyx + vec3(0, 2, 4));
+void main()
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = gl_FragCoord.xy/resolution.xy;
 
-    // Вывод цвета на экран
-    fragColor = vec4(col, 1.0);
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*tan(time+uv.xyx+vec3(0,5,4));
+
+    // Output to screen
+    FragColor = vec4(col,1.0);
 }
